edaa1aae700827e1f195d2b3d3c9767147faa520
theluckygod <thieu216777@gmail.com>
2023-03-26 01:27:41 +0700

    modify convert model to tflite

--- convert_tf2tflite.py
+++ convert_tf2tflite.py
@@ -1,56 +1,86 @@
+from model.stgcn import Model
 import tensorflow as tf
 import numpy as np
-from model.stgcn import Model
+import argparse
+import os
 
-BATCH_SIZE = 4
-max_body_true = 2
-max_body_kinect = 4
-num_joint = 25
-max_frame = 300
-# (N, C, T, V, M) = (BATCH_SIZE, 3, max_frame, num_joint, max_body_true) # (BATCH_SIZE, 1, 48, 110, 1)
-(N, C, T, V, M) = (BATCH_SIZE, 3, 48, 115, 1)
+tf.random.set_seed(42)
+max_body_true = 1
+num_joint = 115
+max_frame = 48
+batch_size = 8
 
-model = Model(num_classes=250) 
-inputs = np.random.rand(N, C, T, V, M)
+def get_parser():
+    # parameter priority: command line > config > default
+    parser = argparse.ArgumentParser(
+        description='Spatial Temporal Graph Convolutional Neural Network for Skeleton-Based Action Recognition')
+    parser.add_argument(
+        '--num-classes', type=int, default=250, help='number of classes in dataset')
+    parser.add_argument(
+        '--checkpoint-path',
+        default="checkpoints/asl_data",
+        help='folder to store model weights')
 
-tf_output = model(inputs, training=False)
-
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-
-converter.target_spec.supported_ops = [
-    tf.lite.OpsSet.TFLITE_BUILTINS, # enable TensorFlow Lite ops.
-    tf.lite.OpsSet.SELECT_TF_OPS # enable TensorFlow ops.
-]
-converter.experimental_new_converter=True
-converter.allow_custom_ops=True
-
-tflite_model = converter.convert()
-with open('model.tflite', 'wb') as f:
-    f.write(tflite_model)
+    return parser
 
 
-interpreter = tf.lite.Interpreter(model_path="model.tflite")
-interpreter.allocate_tensors()
+if __name__ == "__main__":
+    parser = get_parser()
+    arg = parser.parse_args()
 
-# Get input and output tensors.
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+    num_classes     = arg.num_classes
+    checkpoint_path = arg.checkpoint_path
+    strategy        = tf.distribute.MirroredStrategy()
 
-# Test the model on random input data.
-input_shape = input_details[0]['shape']
 
-tf_input_b1 = inputs[:1, ...]
-tf_output_b1 = tf_output[:1, :]
-tflite_inputs = tf_input_b1.astype(np.float32)
+    with strategy.scope():
+        model = Model(num_classes=num_classes)
+        ckpt         = tf.train.Checkpoint(model=model)
+        ckpt_manager = tf.train.CheckpointManager(ckpt,
+                                                  checkpoint_path,
+                                                  max_to_keep=5)
+        ckpt.restore(ckpt_manager.latest_checkpoint).expect_partial()
 
-interpreter.set_tensor(input_details[0]['index'], tflite_inputs)
-interpreter.invoke()
 
-# The function `get_tensor()` returns a copy of the tensor data.
-# Use `tensor()` in order to get a pointer to the tensor.
-tflite_outputs = interpreter.get_tensor(output_details[0]['index'])
+    (N, C, T, V, M) = (batch_size, 3, max_frame, num_joint, max_body_true)
+    inputs = np.random.rand(N, C, T, V, M)
 
-print("input_distance:", tf.norm(tf_input_b1 - tflite_inputs))
-print("output_distance:", tf.norm(tf_output_b1 - tflite_outputs))
+    tf_output = model(inputs, training=False)
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.target_spec.supported_ops = [
+        tf.lite.OpsSet.TFLITE_BUILTINS, # enable TensorFlow Lite ops.
+        tf.lite.OpsSet.SELECT_TF_OPS # enable TensorFlow ops.
+    ]
+    converter.experimental_new_converter=True
+    converter.allow_custom_ops=True
+
+    tflite_model = converter.convert()
+    model_path = os.path.join(checkpoint_path, 'model.tflite')
+    with open(model_path, 'wb') as f:
+        f.write(tflite_model)
+
+
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Test the model on random input data.
+    input_shape = input_details[0]['shape']
+
+    tf_input_b1 = inputs[:1, ...]
+    tf_output_b1 = tf_output[:1, :]
+    tflite_inputs = tf_input_b1.astype(np.float32)
+
+    interpreter.set_tensor(input_details[0]['index'], tflite_inputs)
+    interpreter.invoke()
+
+    tflite_outputs = interpreter.get_tensor(output_details[0]['index'])
+
+    print("input_distance:", tf.norm(tf_input_b1 - tflite_inputs))
+    print("output_distance:", tf.norm(tf_output_b1 - tflite_outputs))
 
 
